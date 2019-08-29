7aee31cb854efa4cb669558bab7c3aa9a74dbb91
dailu1997 <814705056@qq.com>
2019-08-29 20:05:45 +0800

    图片

--- pages/index/brand/brand.js
+++ pages/index/brand/brand.js
@@ -10,6 +10,12 @@ Page({
       { img: 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640', index: 1 },
       { img: 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640', index: 2 }
     ],
+    imgArr: [
+      'http://bpic.588ku.com/element_origin_min_pic/16/10/30/528aa13209e86d5d9839890967a6b9c1.jpg',
+      'http://bpic.588ku.com/element_origin_min_pic/16/10/30/54fcef525fa8f6037d180f3c26f3be65.jpg',
+      'http://bpic.588ku.com/element_origin_min_pic/16/10/30/62e3ca3a02dddb002eff00482078d194.jpg',
+      'http://bpic.588ku.com/element_origin_min_pic/16/10/31/c7167fcfb4ebcd12621c05b0c852e98e.jpg'
+    ]
   },
 
   /**
@@ -17,7 +23,21 @@ Page({
    */
   onLoad: function (options) {
     var id = options.id
+    var name = options.name
+    if(name == 'huxing') {
+      this.setData({
+        img:this.data.imgUrls[0].img
+      })
+    }
     if(id==0) {
+      this.setData({
+        img:this.data.imgUrls[id].img
+      })
+    }else if(id==1){
+      this.setData({ 
+        img:this.data.imgUrls[id].img
+      })
+    }else {
       this.setData({
         img:this.data.imgUrls[id].img
       })
@@ -26,6 +46,18 @@ Page({
       id:options.id,
       name:options.name
     });
+    
+  },
+  imgClick: function () {
+    //  var imgArr = this.data.imgArr;
+    var arr = [];
+     for (var i = 0; i < this.data.imgUrls.length; i++) {
+       arr.push(this.data.imgUrls[i].img)
+     }
+     wx.previewImage({
+      current: arr[0],     //当前图片地址
+      urls: arr,   
+     })        
   },
 
   /**
